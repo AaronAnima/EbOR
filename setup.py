b019c6a18402fa08655324763da687e8dde5b6a0
AaronAnima <AaronAnima@github.com>
2023-04-03 15:43:31 +0800

    test new branch

--- setup.py
+++ setup.py
@@ -9,29 +9,29 @@ with open('README.md', 'r') as f:
     # description from readme file
     long_description = f.read()
 
-# setup(
-#     name='ebor',
-#     version='0.1',
-#     author='Mingdong Wu',
-#     author_email='wmingd@pku.edu.cn',
-#     description='Environments for Example-based Object Rearrangement.',
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     packages=find_packages(),
-#     license='MIT license',
-#     url='https://github.com/AaronAnima/EbOR',
-#     install_requires=[
-#         'gym>=0.25.2',
-#         'numpy',
-#         'pybullet>=3.2.5',
-#     ],
-#     python_requires='>=3.6',
-#     platforms=['Linux Ubuntu'],  
-#     classifiers=[
-#         'Programming Language :: Python :: 3',
-#         'Programming Language :: Python :: 3.6',
-#         'Programming Language :: Python :: 3.7',
-#         'Programming Language :: Python :: 3.8',
-#         'Programming Language :: Python :: 3.9',
-#     ],
-# )
+setup(
+    name='ebor',
+    version='0.1',
+    author='Mingdong Wu',
+    author_email='wmingd@pku.edu.cn',
+    description='Environments for Example-based Object Rearrangement.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license='MIT license',
+    url='https://github.com/AaronAnima/EbOR',
+    install_requires=[
+        'gym>=0.25.2',
+        'numpy',
+        'pybullet>=3.2.5',
+    ],
+    python_requires='>=3.6',
+    platforms=['Linux Ubuntu'],  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
